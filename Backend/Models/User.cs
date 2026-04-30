3d603f845b469291e8230b4c61373929d5d610d0
MinhVuong2203 <vuonghihihihi@gmail.com>
2026-04-30 17:48:44 +0700

    Test connect Firebase

---
+++ Backend/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Google.Cloud.Firestore;
+
+[FirestoreData]
+public class User
+{
+    [FirestoreProperty] public string displayName { get; set; }
+    [FirestoreProperty] public string email { get; set; }
+    [FirestoreProperty] public string role { get; set; }
+    [FirestoreProperty] public string status { get; set; }
+    [FirestoreProperty] public string uid { get; set; }
+}
