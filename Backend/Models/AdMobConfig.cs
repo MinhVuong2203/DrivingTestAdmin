6626788f5fc281be3cf0c0c464211f4a63e76349
Vanhung0611 <vanhungvn741@gmail.com>
2026-05-29 17:29:27 +0700

    feat: add AdMob API controller key

---
+++ Backend/Models/AdMobConfig.cs
@@ -0,0 +1,7 @@
+﻿public class AdMobConfig
+{
+    public string AppId { get; set; } = "";
+    public string BannerId { get; set; } = "";
+    public string InterstitialId { get; set; } = "";
+    public string RewardedId { get; set; } = "";
+}
